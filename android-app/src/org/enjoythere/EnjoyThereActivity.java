2c6dad69ea24450d08696e0c0e5550679a0b2d11
flauer <bkwiatkowski@10g.pl>
2012-03-24 13:21:28 +0100

    places

--- android-app/src/org/enjoythere/EnjoyThereActivity.java
+++ android-app/src/org/enjoythere/EnjoyThereActivity.java
@@ -1,13 +1,20 @@
 package org.enjoythere;
 
+import com.parse.Parse;
+import com.parse.ParseObject;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class EnjoyThereActivity extends Activity {
 
+	EditText login;
+	Button loginBtn;
 	Button scanBtn;
 	Button searchBtn;
 	Button myPlacesBtn;
@@ -17,13 +24,49 @@ public class EnjoyThereActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         
+        Parse.initialize(this, "bOBUxCViaQbtvA3hTsljZ501zfjEL5jXbefXPR2j", 
+        		"Wmd7MO4Wa86GVN7c7LiZVDhGF575lc0ni13pttVE");
+        
+//        ParseObject testObject = new ParseObject("TestObject");
+//        testObject.put("foo", "bar");
+//        testObject.saveInBackground();
+        
         myPlacesBtn = (Button) findViewById(R.id.myPlacesBtn);
+        loginBtn = (Button) findViewById(R.id.loginBtn);
+        login = (EditText) findViewById(R.id.login);
+        
+        Preferences pref = Preferences.Instance();
+        pref.Initialize(getApplicationContext());
+        
+        if (pref.GetLogin() != null) {
+        	login.setText(pref.GetLogin());
+        }
+        
+        loginBtn.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				Preferences pref = Preferences.Instance();
+		        pref.Initialize(getApplicationContext());
+				pref.SetCredentials(login.getText().toString(), "");
+				pref.Save();
+			}
+		});
         
         myPlacesBtn.setOnClickListener(new OnClickListener() {
 			
 			public void onClick(View arg0) {
 				
 				
+			}
+		});
+        
+        scanBtn = (Button) findViewById(R.id.scanBtn);
+        
+        scanBtn.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View arg0) {
+				Intent scan  = new Intent(getApplicationContext(), PlaceActivity.class);
+				startActivity(scan);
 			}
 		});
     }
--- android-app/src/org/enjoythere/PlaceActivity.java
+++ android-app/src/org/enjoythere/PlaceActivity.java
@@ -1,29 +1,48 @@
 package org.enjoythere;
 
+import com.parse.ParseObject;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 public class PlaceActivity extends Activity {
 
 	Button sendBtn;
+	EditText placeName;
+	EditText placeDesc;
 	RatingBar ratingBar;
-	Button myPlacesBtn;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.place);
         
-        myPlacesBtn = (Button) findViewById(R.id.myPlacesBtn);
+        placeName = (EditText) findViewById(R.id.placeName);
+        placeDesc = (EditText) findViewById(R.id.placeDesc);
+        ratingBar = (RatingBar) findViewById(R.id.placeratingBar);
         
-        myPlacesBtn.setOnClickListener(new OnClickListener() {
+        sendBtn = (Button) findViewById(R.id.placeSendBtn);
+        
+        sendBtn.setOnClickListener(new OnClickListener() {
 			
 			public void onClick(View arg0) {
-				
-				
+				if (placeName.getText() != null &&
+						placeDesc.getText() != null) {
+					
+					ParseObject testObject = new ParseObject("Place");
+			        testObject.put("placeName", placeName.getText().toString());
+			        testObject.put("placeDesc", placeDesc.getText().toString());
+			        testObject.saveInBackground();
+				} else {
+					Toast.makeText(getApplicationContext(), 
+							"Wype³nij wszystkie dane!", Toast.LENGTH_SHORT);
+				}
 			}
 		});
     }
