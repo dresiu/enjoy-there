0d758d3173cd5482ce400911102665d747d8201d
dresiu <mcendrju@gmail.com>
2012-03-24 16:14:12 +0100

    widok statystyki

---
+++ android-app/src/org/enjoythere/PlaceReportActivity.java
@@ -0,0 +1,127 @@
+package org.enjoythere;
+
+import java.util.Arrays;
+import java.util.Date;
+
+import java.util.List;
+import java.util.regex.Pattern;
+
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.PatternMatcher;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class PlaceReportActivity extends Activity {
+
+	Button sendBtn;
+	TextView placeName;
+	TextView placeDesc;
+	RatingBar ratingBar;
+	ImageView image;
+	TextView occupancy;
+	String [] occVals;
+
+	String placeId;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.place);
+		placeId = this.getIntent().getStringExtra("PLACE_NAME");
+		boolean newPlace = true;
+		if (newPlace) {
+			Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+			intent.putExtra("com.google.zxing.client.android.SCAN.SCAN_MODE",
+					"QR_CODE_MODE");
+			startActivityForResult(intent, 0);
+		}
+
+		placeName = (EditText) findViewById(R.id.report_placeName);
+		placeDesc = (EditText) findViewById(R.id.report_placeDesc);
+		ratingBar = (RatingBar) findViewById(R.id.report_placeratingBar);
+		occupancy = (TextView) findViewById(R.id.report_occupancy);
+		occVals = getResources().getStringArray(R.array.occupancy_array);
+	    
+		loadPlace();
+
+		
+	}
+
+
+
+	private void loadPlace() {
+		ParseQuery query = new ParseQuery("Place");
+		query.whereEqualTo("objectId", placeId);
+		try {
+			List<ParseObject> places = query.find();
+			placeName.setText(places.get(0).getString("name"));
+			placeDesc.setText(places.get(0).getString("placeDesc"));
+
+		} catch (ParseException e) {
+			Log.d("Place", "Error: " + e.getMessage());
+		}
+		
+		query = new ParseQuery("UserPlace");
+		query.whereEqualTo("name", placeName.getText());
+		try {
+			List<ParseObject> places = query.find();
+			
+			int  [] occupancyCount = new int[4];
+			Arrays.fill(occupancyCount, 0);
+			
+			double ratingSum = 0;
+			
+			for(ParseObject obj : places){
+				String itemOcc = obj.getString("currOccupancy");
+				for(int i = 0; i < 4; i++){
+					if(itemOcc.equals(occVals[i])){
+						occupancyCount[i]++;
+					}
+				}
+				
+				ratingSum += obj.getDouble("myRating");
+					
+			}
+			
+			int maxInd = 0;
+			int val = occupancyCount[0];
+			
+			for(int i = 0; i < 4; i++){
+				if(occupancyCount[i] > val)
+				{
+					val = occupancyCount[i];
+					maxInd = i;
+				}
+				
+			}
+
+			occupancy.setText("oblezenie: "+occVals[maxInd]);
+			ratingBar.setRating((float)(ratingSum/places.size()));
+
+		} catch (ParseException e) {
+			Log.d("Place", "Error: " + e.getMessage());
+		}
+		
+		
+
+	}
+	
+
+}
