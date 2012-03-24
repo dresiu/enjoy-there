4d54ba3fba9fb619a993087f61c9f08bd424604a
flauer <bkwiatkowski@10g.pl>
2012-03-24 12:08:05 +0100

    widok listy#

--- android-app/src/org/enjoythere/EnjoyThereActivity.java
+++ android-app/src/org/enjoythere/EnjoyThereActivity.java
@@ -2,12 +2,29 @@ package org.enjoythere;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class EnjoyThereActivity extends Activity {
-    /** Called when the activity is first created. */
+
+	Button scanBtn;
+	Button searchBtn;
+	Button myPlacesBtn;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        myPlacesBtn = (Button) findViewById(R.id.myPlacesBtn);
+        
+        myPlacesBtn.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View arg0) {
+				
+				
+			}
+		});
     }
 }
